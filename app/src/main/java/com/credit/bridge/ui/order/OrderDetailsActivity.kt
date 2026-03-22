7d39cee6ddb1967a9d6d527f1c05cd56a0400cb9
Jason <qijs@shinetechsoftware.com>
2026-03-22 23:44:57 +0800

    feat: add image res

--- app/src/main/java/com/credit/bridge/ui/order/OrderDetailsActivity.kt
+++ app/src/main/java/com/credit/bridge/ui/order/OrderDetailsActivity.kt
@@ -1,21 +1,28 @@
 package com.credit.bridge.ui.order
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.credit.bridge.R
+import com.credit.bridge.base.BaseActivity
+import com.credit.bridge.databinding.ActivityLoginBinding
+import com.credit.bridge.databinding.ActivityOrderDetailsBinding
 
-class OrderDetailsActivity : AppCompatActivity() {
+class OrderDetailsActivity : BaseActivity<ActivityOrderDetailsBinding>(), View.OnClickListener {
+
+    override fun getBinding() = ActivityOrderDetailsBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_order_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+
+    override fun initRes() {
+        super.initRes()
+    }
+
+    override fun onClick(v: View?) {
     }
 }
