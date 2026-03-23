94ca6414d6564cb7378f55cad17727534aaca126
Jason <qijs@shinetechsoftware.com>
2026-03-23 23:37:40 +0800

    feat: edit bank page

--- app/src/main/java/com/credit/bridge/ui/order/EditCardActivity.kt
+++ app/src/main/java/com/credit/bridge/ui/order/EditCardActivity.kt
@@ -1,21 +1,44 @@
 package com.credit.bridge.ui.order
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.credit.bridge.R
+import com.credit.bridge.base.BaseActivity
+import com.credit.bridge.databinding.ActivityEditCardBinding
+import com.credit.bridge.databinding.ActivityOrderDetailsBinding
+import com.credit.bridge.util.ToastUtil
 
-class EditCardActivity : AppCompatActivity() {
+class EditCardActivity : BaseActivity<ActivityEditCardBinding>(), View.OnClickListener {
+
+
+    override fun getBinding() = ActivityEditCardBinding.inflate(layoutInflater)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_card)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+    }
+
+override fun initRes() {
+    super.initRes()
+
+    bindViews.titleLayout.backIv.setOnClickListener(this)
+    bindViews.titleLayout.titleTv.setOnClickListener(this)
+    bindViews.titleLayout.titleTv.text = "Details"
+}
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.backIv -> {
+                finish()
+            }
+            R.id.titleTv -> {
+                ToastUtil.showShort(this, "Right")
+            }
         }
     }
 }
