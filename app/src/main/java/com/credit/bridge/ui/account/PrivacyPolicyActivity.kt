0915cbd5d63232f3ae0aacee43d66e554389144f
Jason <qijs@shinetechsoftware.com>
2026-05-15 23:44:45 +0800

    perf： privacy load

--- app/src/main/java/com/credit/bridge/ui/account/PrivacyPolicyActivity.kt
+++ app/src/main/java/com/credit/bridge/ui/account/PrivacyPolicyActivity.kt
@@ -1,23 +1,29 @@
 package com.credit.bridge.ui.account
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.credit.bridge.R
 import com.credit.bridge.base.BaseActivity
 import com.credit.bridge.databinding.ActivityPrivacyPolicyBinding
-import com.credit.bridge.databinding.ActivityProductListBinding
 import com.credit.bridge.remote.HttpClient
 import com.credit.bridge.remote.event.PolicyLinkResponseEvent
 import com.credit.bridge.util.ToastUtil
 import com.squareup.otto.Subscribe
 
-class PrivacyPolicyActivity : BaseActivity<ActivityPrivacyPolicyBinding>(), View.OnClickListener  {
+class PrivacyPolicyActivity : BaseActivity<ActivityPrivacyPolicyBinding>(), View.OnClickListener {
 
     override fun getBinding() = ActivityPrivacyPolicyBinding.inflate(layoutInflater)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,36 +33,68 @@ class PrivacyPolicyActivity : BaseActivity<ActivityPrivacyPolicyBinding>(), View
         super.initRes()
 
         bindViews.titleLayout.titleTv.text = "Privacy Policy"
-
         bindViews.titleLayout.backLl.setOnClickListener(this)
 
+        setupWebView()
         getPolicyLink()
     }
 
-    private fun getPolicyLink(){
+    private fun getPolicyLink() {
         showLoading()
         HttpClient.getPolicyLink(this)
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
+        bindViews.webView.apply {
+            setBackgroundColor(Color.WHITE)
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            webChromeClient = WebChromeClient()
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    hideLoading()
+                }
+
+                override fun onReceivedError(
+                    view: WebView?,
+                    request: WebResourceRequest?,
+                    error: WebResourceError?
+                ) {
+                    super.onReceivedError(view, request, error)
+                    if (request?.isForMainFrame == true) {
+                        hideLoading()
+                    }
+                }
+            }
+        }
+    }
+
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.backLl -> {
-                finish()
-            }
+            R.id.backLl -> finish()
         }
     }
 
     @Subscribe
     fun onPolicyLinkResponseEvent(event: PolicyLinkResponseEvent) {
-        hideLoading()
-        if(event.isSuccess){
-            event.model?.mtaw?.let {
-                bindViews.webView.loadUrl(it)
+        if (event.isSuccess) {
+            val url = event.model?.mtaw?.trim().orEmpty()
+            if (url.isNotEmpty()) {
+                bindViews.webView.loadUrl(url)
+            } else {
+                hideLoading()
+                ToastUtil.showLong(this, event.retMsg)
             }
-        }else{
-            ToastUtil.showLong(this,event.retMsg)
+        } else {
+            hideLoading()
+            ToastUtil.showLong(this, event.retMsg)
         }
     }
 
-
+    override fun onDestroy() {
+        bindViews.webView.stopLoading()
+        super.onDestroy()
+    }
 }
