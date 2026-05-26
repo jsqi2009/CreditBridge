c244b32fa83f88b6fbfa203972206faefe3dc799
Jason <qijs@shinetechsoftware.com>
2026-05-27 00:08:10 +0800

    perf

--- app/src/main/java/com/credit/bridge/ui/order/EditCardActivity.kt
+++ app/src/main/java/com/credit/bridge/ui/order/EditCardActivity.kt
@@ -1,18 +1,16 @@
 package com.credit.bridge.ui.order
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.credit.bridge.R
 import com.credit.bridge.base.BaseActivity
 import com.credit.bridge.databinding.ActivityEditCardBinding
-import com.credit.bridge.databinding.ActivityOrderDetailsBinding
-import com.credit.bridge.inter.OnSelectListener
 import com.credit.bridge.remote.HttpClient
 import com.credit.bridge.remote.body.RequestBankInfoBody
 import com.credit.bridge.remote.event.FetchBankInfoResponseEvent
@@ -20,18 +18,15 @@ import com.credit.bridge.remote.event.VerifyBankInfoResponseEvent
 import com.credit.bridge.remote.response.BankInfo
 import com.credit.bridge.util.NumberUtils
 import com.credit.bridge.util.ToastUtil
-import com.credit.bridge.util.VerifyInfoUtil
-import com.credit.bridge.widget.PermissionBottomSheet
 import com.credit.bridge.widget.VerifyBankInfoBottomSheet
 import com.squareup.otto.Subscribe
 
 class EditCardActivity : BaseActivity<ActivityEditCardBinding>(), View.OnClickListener {
 
-
     override fun getBinding() = ActivityEditCardBinding.inflate(layoutInflater)
 
-
     private var bankInfo: BankInfo? = null
+    private var editCardKeyboardHandlingActive = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,7 +47,81 @@ class EditCardActivity : BaseActivity<ActivityEditCardBinding>(), View.OnClickLi
         bindViews.confirmNewAccountEt.addTextChangedListener(confirmNewAccountTextWatcher)
         bindViews.newIfscEt.addTextChangedListener(newIfscTextWatcher)
 
+        enableEditCardKeyboardHandling()
         getBankInfo()
+    }
+
+    override fun onDestroy() {
+        disableEditCardKeyboardHandling()
+        super.onDestroy()
+    }
+
+    private fun enableEditCardKeyboardHandling() {
+        if (editCardKeyboardHandlingActive) return
+        editCardKeyboardHandlingActive = true
+
+        val root = bindViews.main
+        val defaultBottom = resources.getDimensionPixelSize(R.dimen.bottom_menu_height_64)
+        root.setPadding(0, 0, 0, defaultBottom)
+
+        ViewCompat.setOnApplyWindowInsetsListener(root) { v, insets ->
+            val imeBottom = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom
+            val navBottom = insets.getInsets(WindowInsetsCompat.Type.navigationBars()).bottom
+            v.setPadding(0, 0, 0, maxOf(imeBottom, navBottom, defaultBottom))
+            scheduleEditCardFocusedFieldScroll()
+            insets
+        }
+        ViewCompat.requestApplyInsets(root)
+
+        val focusScroll = View.OnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                scrollEditCardFieldIntoView(v)
+            }
+        }
+        bindViews.currentAccountEt.onFocusChangeListener = focusScroll
+        bindViews.newAccountEt.onFocusChangeListener = focusScroll
+        bindViews.confirmNewAccountEt.onFocusChangeListener = focusScroll
+        bindViews.newIfscEt.onFocusChangeListener = focusScroll
+    }
+
+    private fun disableEditCardKeyboardHandling() {
+        if (!editCardKeyboardHandlingActive) return
+        editCardKeyboardHandlingActive = false
+
+        val root = bindViews.main
+        ViewCompat.setOnApplyWindowInsetsListener(root, null)
+        root.setPadding(0, 0, 0, resources.getDimensionPixelSize(R.dimen.bottom_menu_height_64))
+        bindViews.currentAccountEt.onFocusChangeListener = null
+        bindViews.newAccountEt.onFocusChangeListener = null
+        bindViews.confirmNewAccountEt.onFocusChangeListener = null
+        bindViews.newIfscEt.onFocusChangeListener = null
+        ViewCompat.requestApplyInsets(root)
+    }
+
+    private fun scheduleEditCardFocusedFieldScroll() {
+        val focused = currentFocus ?: return
+        if (focused != bindViews.currentAccountEt &&
+            focused != bindViews.newAccountEt &&
+            focused != bindViews.confirmNewAccountEt &&
+            focused != bindViews.newIfscEt
+        ) {
+            return
+        }
+        bindViews.editCardScrollView.postDelayed({
+            scrollEditCardFieldIntoView(focused)
+        }, 80)
+    }
+
+    private fun scrollEditCardFieldIntoView(focused: View) {
+        val scrollView = bindViews.editCardScrollView
+        scrollView.post {
+            val content = scrollView.getChildAt(0) ?: return@post
+            val rect = Rect()
+            focused.getDrawingRect(rect)
+            scrollView.offsetDescendantRectToMyCoords(focused, rect)
+            rect.bottom += resources.getDimensionPixelSize(R.dimen.margin_20)
+            scrollView.requestChildRectangleOnScreen(content, rect, true)
+        }
     }
 
     override fun onClick(v: View?) {
@@ -74,19 +143,19 @@ class EditCardActivity : BaseActivity<ActivityEditCardBinding>(), View.OnClickLi
     @Subscribe
     fun onFetchBankInfoResponseEvent(event: FetchBankInfoResponseEvent) {
         hideLoading()
-        if(event.isSuccess){
+        if (event.isSuccess) {
             bankInfo = event.model?.mtaw
             if (!bankInfo?.rcpqzqrn.isNullOrEmpty()) {
                 bindViews.ifscTv.text = getString(R.string.product_ifsc) + " " +
-                        NumberUtils.formatNumber(bankInfo?.rcpqzqrn,3,2)
+                    NumberUtils.formatNumber(bankInfo?.rcpqzqrn, 3, 2)
             }
             if (!bankInfo?.qmtddx.isNullOrEmpty()) {
                 bindViews.accountTv.text = getString(R.string.product_account) + " " +
-                        NumberUtils.formatNumber(bankInfo?.qmtddx,3,2)
+                    NumberUtils.formatNumber(bankInfo?.qmtddx, 3, 2)
                 bindViews.currentAccountEt.setText(bankInfo?.qmtddx)
             }
-        }else{
-            ToastUtil.showLong(this,event.networkError.toString())
+        } else {
+            ToastUtil.showLong(this, event.networkError.toString())
         }
     }
 
@@ -113,15 +182,15 @@ class EditCardActivity : BaseActivity<ActivityEditCardBinding>(), View.OnClickLi
             ToastUtil.showLong(this, "Please provide a valid IFSC code")
             return
         }
-        if (currentAccount.replace(" ","") != bankInfo?.qmtddx) {
+        if (currentAccount.replace(" ", "") != bankInfo?.qmtddx) {
             ToastUtil.showLong(this, "Please enter your current bank account number")
             return
         }
-        if (newAccount.replace(" ","") != confirmNewAccount.replace(" ","")) {
+        if (newAccount.replace(" ", "") != confirmNewAccount.replace(" ", "")) {
             ToastUtil.showLong(this, "The account numbers entered do not match")
             return
         }
-        if (newIfsc.replace(" ","").length != 11) {
+        if (newIfsc.replace(" ", "").length != 11) {
             ToastUtil.showLong(this, "IFSC code should contain 11 characters")
             return
         }
@@ -137,7 +206,7 @@ class EditCardActivity : BaseActivity<ActivityEditCardBinding>(), View.OnClickLi
         verifyBankInfoBottomSheet.show(supportFragmentManager, "workTypeSheet")
     }
 
-    private fun updateBankInfo(code: String,ifsc: String,account: String) {
+    private fun updateBankInfo(code: String, ifsc: String, account: String) {
 
         val body = RequestBankInfoBody()
         body.jmxiec = account
@@ -147,7 +216,6 @@ class EditCardActivity : BaseActivity<ActivityEditCardBinding>(), View.OnClickLi
         body.fzxl = code
 
         HttpClient.updateBankInfo(this, body)
-
     }
 
     @Subscribe
@@ -156,10 +224,10 @@ class EditCardActivity : BaseActivity<ActivityEditCardBinding>(), View.OnClickLi
         if (event.isSuccess) {
             finish()
         } else {
-            if(event.model != null && event.model?.fzpn == 500){
-                ToastUtil.showLong(this,event.model?.dvusonb)
-            }else{
-                ToastUtil.showLong(this,event.networkError.toString())
+            if (event.model != null && event.model?.fzpn == 500) {
+                ToastUtil.showLong(this, event.model?.dvusonb)
+            } else {
+                ToastUtil.showLong(this, event.networkError.toString())
             }
         }
     }
